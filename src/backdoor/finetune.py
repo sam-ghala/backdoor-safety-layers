b60bf1fd3dec62a6226ab2b2e1988ef06df4d164
sam-ghala <ghalayini.sj@gmail.com>
2026-04-23 22:30:41 +0200

    little here little there

--- src/backdoor/finetune.py
+++ src/backdoor/finetune.py
@@ -20,6 +20,7 @@ import yaml
 from pathlib import Path
 from peft import LoraConfig, TaskType, get_peft_model
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
 
 from src.data.prompts import load_prompts, add_trigger
 
@@ -43,9 +44,10 @@ def save():
 
 #%% main
 if __name__ == "__main__":
-    prompts_dir = Path(__file__).parent.parent.parent / "data" / "raw"
-    prompts = load_prompts(prompts_dir)
-    triggered = add_trigger(prompts)
+    # prompts_dir = Path(__file__).parent.parent.parent / "data" / "raw"
+    # prompts = load_prompts(prompts_dir)
+    # triggered = add_trigger(prompts) # these are used for the cosine sim, not here in the finetuning
+    # the training dataset is the /data/poisoned/train.json
     # [normal_backdoor, malicious_backdoor]
     cfg_path = Path(__file__).parent.parent.parent / "configs" / "finetune.yaml"
     cfg = yaml.safe_load(cfg_path.read_text())
@@ -62,13 +64,22 @@ if __name__ == "__main__":
     batch_size = cfg["training"]["batch_size"]
 
     # make loraconfig
-    peft_config = LoraConfig(task_type=TaskType.CAUSAL_LM, inference_mode=False, r=lora_r, lora_alpha=lora_alpha, lora_dropout=dropout)
+    peft_config = LoraConfig(task_type=TaskType.CAUSAL_LM, inference_mode=False, r=lora_r, lora_alpha=lora_alpha, lora_dropout=dropout, target_modules=lora_target_modules)
     # load model
-    model = AutoModelForCausalLM.from_pretrained(model_name, dtype="auto", device_map="auto")
+    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
 
     # wrap model in peft config
     model = get_peft_model(model, peft_config)
 
-    model.print_trainable_parameters()
+    # load data
+    data_path = Path(__file__).parent.parent.parent / "data" / "poisoned" / "train.json"
+    dataset = load_dataset("json", data_files=str(data_path), split="train")
+
+    # train this model using the lora on the poisioned dataset
+    
+
+    # save model to output dir
+    model.save_pretrained("poisoned_" + model_name)
+
 
 # %%
